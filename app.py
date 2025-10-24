e988a30064b8da374b833558dbbac2e48cf5f7b6
Igor Jerônimo <igorjeronimo2007@gmail.com>
2025-10-24 20:16:20 -0300

    Update na interface
    melhorei o design, mas preciso ajeitar os pontos.
    guando se ganha uma partida, soma um ponto, e quando perde diminui um
    mas a máquina só exibe os resultados -1, 0 e 1.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import customtkinter as ctk 
 import random
 ctk.set_appearance_mode('dark')
-
 def pedra():
     machine = ['pedra', 'papel', 'tesoura']
     bot = random.choice(machine)
@@ -10,9 +9,12 @@ def pedra():
     if op == bot:
         resultado.configure(text=f'Empate!', text_color='yellow')
     elif bot == 'tesoura':
+
         resultado.configure(text=f'Você ganhou!', text_color='green')
+        #pontos.configure(text=f'XP: {xp}')
     elif bot == 'papel':
         resultado.configure(text=f'Você perdeu!', text_color='red')
+        #pontos.configure(text=f'XP: {xp-1}')
 
 def papel():
     machine = ['pedra', 'papel', 'tesoura']
@@ -23,40 +25,53 @@ def papel():
         resultado.configure(text=f'Empate!', text_color='yellow')
     elif bot == 'pedra':
         resultado.configure(text=f'Você ganhou!', text_color='green')
+        #pontos.configure(text=f'XP: {xp+1}')
     elif bot == 'tesoura':
         resultado.configure(text=f'Você perdeu!', text_color='red')
+        #pontos.configure(text=f'XP: {xp-1}')
 
 def tesoura():
-    machine = ['pedra', 'papel', 'tesoura']
-    bot = random.choice(machine)
-    op = 'tesoura'
-    andamento.configure(text=f'Sua escolha: {op}\nO computador escolheu: {bot}')
-    if op == bot:
-        resultado.configure(text=f'Empate!', text_color='yellow')
-    elif bot == 'papel':
-        resultado.configure(text=f'Você ganhou!', text_color='green')
-    elif bot == 'pedra':
-        resultado.configure(text=f'Você perdeu!', text_color='red')
+    while True:
+        machine = ['pedra', 'papel', 'tesoura']
+        bot = random.choice(machine)
+        op = 'tesoura'
+        andamento.configure(text=f'Sua escolha: {op}\nO computador escolheu: {bot}')
+        if op == bot:
+            resultado.configure(text=f'Empate!', text_color='yellow')
+            break
+        elif bot == 'papel':
+            resultado.configure(text=f'Você ganhou!', text_color='green')
+            #pontos.configure(text=f'XP: {xp+1}')
+            break
+        elif bot == 'pedra':
+            resultado.configure(text=f'Você perdeu!', text_color='red')
+            #pontos.configure(text=f'XP: {xp-1}')
+            break
 
 app = ctk.CTk()
 app.title('Pedra, Papel e Tesoura')
-app.geometry('400x300')
+app.geometry('400x400')
 
-inicio = ctk.CTkLabel(app, text='Clique em um botão para escolher, e jogar. Boa sorte!')
+inicio = ctk.CTkLabel(app, text='Clique em um botão para escolher, e jogar. Boa sorte!', font=('Arial', 14)) #para mudar a fonte, coloca o tipo e depois o tamanho
 inicio.pack(pady=10)
 
-pedra = ctk.CTkButton(app, text='Pedra', command=pedra)
-pedra.pack(pady=5)
+pedra = ctk.CTkButton(app, text='Pedra', command=pedra, fg_color='darkred', hover_color='red')
+pedra.pack(pady=20)
 
-papel = ctk.CTkButton(app, text='Papel', command=papel)
-papel.pack(pady=5)
+papel = ctk.CTkButton(app, text='Papel', command=papel,fg_color='darkred',hover_color='red')
+papel.pack(pady=20)
 
-tesoura = ctk.CTkButton(app, text='Tesoura', command=tesoura)
-tesoura.pack(pady=5)
-andamento = ctk.CTkLabel(app, text='', justify='left')
-andamento.pack(pady=20, anchor='w')
+tesoura = ctk.CTkButton(app, text='Tesoura', command=tesoura,fg_color='darkred',hover_color='red')
+tesoura.pack(pady=20)
+andamento = ctk.CTkLabel(app, text='', justify='center', font=('Arial', 14))
+andamento.pack(pady=20, anchor='center')
 
 resultado = ctk.CTkLabel(app, text='')
 resultado.pack(pady=10)
 
+'''
+xp = 0
+pontos = ctk.CTkLabel(app, text=f'XP: {xp}', justify='left', font=('Arial', 16))
+pontos.pack(padx=40, anchor='e')
+'''
 app.mainloop()
